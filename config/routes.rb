802803555b3236c21ab5f5cd5026ce987af6edab
Noel Delgado <noeldelgadom@myhost.local>
2017-05-25 10:01:19 -0500

    Teams API created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  # Api Section
+  namespace :api do
+    resources :teams
+  end
+
   resources :teams
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
